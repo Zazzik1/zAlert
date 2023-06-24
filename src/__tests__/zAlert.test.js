93724c375f920129491d198919649fa0ec594c32
Mateusz Bilicki <mateuszbilicki34@gmail.com>
2023-06-25 00:26:44 +0200

    Dev (#3)
    * test: show and hide method
    
    * test: added close button and onBlur related tests
    
    * test: prettier

--- src/__tests__/zAlert.test.js
+++ src/__tests__/zAlert.test.js
@@ -74,4 +74,39 @@ describe('zAlert', () => {
             })();
         });
     });
+
+    test('show() method should make alert visible, whereas hide() method should hide it and clear the alert', (done) => {
+        (async () => {
+            await zAlert.show('');
+            expect(getContainer().style.opacity).toBe('1');
+            expect(getContainer().style.display).not.toBe('none');
+            await zAlert.hide();
+            expect(getContainer().style.opacity).toBe('0');
+            expect(getContainer().style.display).toBe('none');
+            expect(getContainer().textContent).toBe('');
+            done();
+        })();
+    });
+
+    test('close button should hide alert', (done) => {
+        (async () => {
+            await zAlert.show();
+            const hideMock = jest.fn(() => Promise.resolve());
+            jest.spyOn(zAlert, 'hide').mockImplementationOnce(hideMock);
+            getContainer().querySelector('.zAlert_close').click();
+            expect(hideMock).toBeCalled();
+            done();
+        })();
+    });
+
+    test('clicking outside of alert should hide alert', (done) => {
+        (async () => {
+            await zAlert.show();
+            const hideMock = jest.fn(() => Promise.resolve());
+            jest.spyOn(zAlert, 'hide').mockImplementationOnce(hideMock);
+            document.body.click();
+            expect(hideMock).toBeCalled();
+            done();
+        })();
+    });
 });
